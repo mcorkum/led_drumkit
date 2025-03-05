86653ca93224fc962031e98e541fcb4bf523f2cb
Michael Corkum <mcorkum@gmail.com>
2025-03-04 20:26:15 -0500

    test

--- midi_test.py
+++ midi_test.py
@@ -1,14 +1,7 @@
 import mido
 
-# List available MIDI input devices
-print("Available MIDI inputs:", mido.get_input_names())
+mido.get_input_names() # Get connected midi device names
 
-# Open the first available MIDI input
-midi_input = mido.open_input(mido.get_input_names()[0])
-
-print("Listening for MIDI drum hits...")
-
-# Listen for MIDI messages
-for msg in midi_input:
-    if msg.type == 'note_on':  # Detect drum hits
-        print(f"Drum Hit Detected! Note: {msg.note} Velocity: {msg.velocity}")
+with mido.open_input('<midi_device_name>') as inport:
+    for message in inport:
+        print(message)
