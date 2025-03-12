ad03e08bca47046023ab877e48cafb83b3fcaf61
Michael Corkum <mcorkum@gmail.com>
2025-03-11 21:10:57 -0400

    change: stuff

--- config/config.py
+++ config/config.py
@@ -3,83 +3,50 @@ import json
 MIDI_UNIT = 'UM-ONE:UM-ONE MIDI 1 20:0'
 STRIP_GPIO_PIN = 18
 LED_COUNT = 150
-PAD_NOTES = [36, 38,48,45,43,27,40,50]
-CYMBAL_NOTES = [49,55,52,57,51,53,59,26,22,44]
-KICK_NOTE = 36
-SNARE_NOTE = 38
-ACCENT_NOTE_1 = 47
-ACCENT_NOTE_2 = 58
 
+# Updated lists so that all intended drum hits (including rims) are processed.
+PAD_NOTES = [36, 38, 40, 48, 50, 45, 47, 43, 58]
+CYMBAL_NOTES = [44, 26, 46, 51, 53, 59, 55, 49]
+
+# Updated MIDI note mapping based on your new mapping:
 MIDI_NOTE_INDEX = {
-    36 : "kick",
-    38 : "snare",
-    40 : "snare",
-    48 : "tom1",
-    50 : "tom1",
-    45 : "tom2",
-    47 : "tom2",
-    43 : "tom3",
-    58 : "tom3",
-    27 : "tom4",
-    49 : "crash1",
-    55 : "crash1",
-    52 : "crash2",
-    57 : "crash2",
-    51 : "ride",
-    53 : "ride",
-    59 : "ride",
-    26 : "hihat",
-    22 : "hihat",
-    44 : "hihat"
+    36: "kick",         # Bass Drum
+    44: "hihat",        # Hat Pedal
+    26: "hihat",        # Hi Hat
+    46: "hihat",        # Hi Hat Edge
+    38: "snare",        # Snare
+    40: "snare",        # Snare Rim
+    48: "tom1",         # Tom 1 (head)
+    50: "tom1",         # Tom 1 - Rim
+    45: "tom2",         # Tom 2 (head)
+    47: "tom2",         # Tom 2 - Rim
+    43: "tom3",         # Floor Tom (head)
+    58: "tom3",         # Floor Tom - Rim
+    51: "ride",         # Ride (head)
+    53: "ride",         # Ride Bell
+    59: "crash2",       # Ride Crash / Crash 2 (separate zone)
+    55: "crash1",       # Crash (dedicated)
+    49: "crash1"        # Crash Bell
 }
 
+# Updated LED zone mapping for only these drums.
+# Here, the LED strip is partitioned into zones that sum to 150 LEDs.
+# You can adjust these ranges as needed for your physical layout.
 LED_DRUM_INDEX = {
-    "kick": [74, 149],     # 76 LEDs
-    "snare": [0, 13],      # 14 LEDs
-    "tom1": [13, 22],      # 10 LEDs
-    "tom2": [22, 30],      # 9 LEDs
-    "tom3": [30, 38],      # 9 LEDs
-    "tom4": [38, 51],      # 14 LEDs
-    "ride": [51, 57],      # 7 LEDs
-    "crash2": [57, 62],    # 6 LEDs
-    "crash1": [62, 68],    # 7 LEDs
-    "hihat": [68, 73]      # 6 LEDs
+    "snare":   [0, 14],    # 14 LEDs for snare
+    "tom1":    [14, 24],   # 10 LEDs for Tom 1 (head & rim)
+    "tom2":    [24, 33],   # 9 LEDs for Tom 2 (head & rim)
+    "tom3":    [33, 48],   # 15 LEDs for Floor Tom (head & rim)
+    "ride":    [48, 54],   # 6 LEDs for Ride
+    "crash2":  [54, 64],   # 10 LEDs for Ride Crash / Crash 2
+    "crash1":  [64, 71],   # 7 LEDs for Crash (dedicated)
+    "hihat":   [71, 77],   # 6 LEDs for Hi Hat (Hat Pedal/Hi Hat/Edge)
+    "kick":    [77, 150]   # 73 LEDs for Bass Drum (kick)
 }
 
 COLOURS = {
-    "red": {
-        "r": 255,
-        "g": 0,
-        "b": 0
-    },
-    "green": {
-        "r": 0,
-        "g": 255,
-        "b": 0
-    },
-    "blue": {
-        "r": 0,
-        "g": 0,
-        "b": 255
-    },
-    "purple": {
-        "r": 200,
-        "g": 0,
-        "b": 200
-    },
-    "orange": {
-        "r": 200,
-        "g": 0,
-        "b": 200
-    },
-    "yellow": {
-        "r": 200,
-        "g": 0,
-        "b": 200
-    },
-    "cyan": {
-        "r": 200,
-        "g": 0,
-        "b": 200
-    }
+    "red": {"r": 255, "g": 0, "b": 0},
+    "green": {"r": 0, "g": 255, "b": 0},
+    "blue": {"r": 0, "g": 0, "b": 255},
+    "purple": {"r": 200, "g": 0, "b": 200},
 }
